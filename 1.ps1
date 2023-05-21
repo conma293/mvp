2b63b2772ce501b0bcc18541147c7c1a95dacc51
conma293 <conma293@gmail.com>
2023-05-21 17:55:55 +0800

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -130,8 +130,8 @@ Write-Host "[*] Find-LocalAdminAccess -CheckAccess"
 Write-Host "`n"
 
 Write-Host "[!] Remember to re-run bloodhound or enum after every new user/machine access!"
-#Write-Host "[*] Invoke-UserHunter for users/groups you want to target - will show all active sessions for users/users of specified groups"
-#Write-Host "[*] Get-NetLoggedon –ComputerName <hostname> (needs localadmin) or similar to hunt a machine"
+Write-Host "[*] Invoke-UserHunter for users/groups you want to target - will show all active sessions for users/users of specified groups"
+Write-Host "[*] Get-NetLoggedon –ComputerName (needs localadmin) or similar to hunt a machine"
 Write-Host "`n"
 
 
